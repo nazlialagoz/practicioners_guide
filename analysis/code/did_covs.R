ef06070983254ac5dc98a0c2fb0ec6bd23cc8847
Nazli Alagoz <nazlialagoz@nazlis-mbp.home>
2023-11-06 17:30:53 +0100

    covariates

--- analysis/code/did_covs.R
+++ analysis/code/did_covs.R
@@ -1,2 +1,122 @@
 # Covariates --------------------------------------------------------------
 # TWFE & ETWFE & Stacked
+rm(list = ls())
+source('sim_data_cov.R') # Import simulation function and some utilities
+source('common_def_func.R') # Import libraries and common functions
+
+dt <- sim_data()
+head(dt)
+
+
+# Prep --------------------------------------------------------------------
+
+
+unique(dt$cohort_period)
+unique(dt$period)
+# dt <- dt[] # exclude period where everyone is treated so that we have an untreated group
+
+# EDA and Analysis --------------------------------------------------------
+# Check out the data
+dt[, .N, by = cohort_period] # group sizes
+dt[, .(mean_dep_var = mean(dep_var)), by = cohort_period]
+
+# Create relative time dummy
+dt[, time_since_treat := period-cohort_period]
+dt[cohort_period>max(period), time_since_treat:= -999] # nontreated group
+dt[, treatment_group:= ifelse(time_since_treat != -999, 1,0)] # nontreated group
+dt[treatment_group == 0, cohort_period := 999]
+sort(unique(dt$time_since_treat))
+
+unique(dt[time_since_treat == -999]$cohort_period)
+
+unq_rel_t <- sort(unique(dt$time_since_treat))
+MAX_WEEKS = max(setdiff(unq_rel_t,-999))
+
+# Find number of units per time since treat
+n_by_time_since_treat <- dt[time_since_treat>0, .N, by = time_since_treat]
+tot_N <- sum(n_by_time_since_treat$N)
+n_by_time_since_treat[, prop := N/tot_N]
+
+# Based on the existence of untreated group decide control group and ref periods
+there_is_untreated = (dt[cohort_period == 999, .N] > 0)
+
+if(there_is_untreated){
+  control_group = c("nevertreated","notyettreated") # CS
+  
+  most_neg_t <- min(setdiff(unq_rel_t,-999))
+  ref_periods <- c(most_neg_t,-999) # TODO: change this into the most min one?
+  
+}else{
+  control_group = c("notyettreated")
+  ref_periods <- unq_rel_t[1:2] # the most two negative
+}
+
+
+
+# Visualization -----------------------------------------------------------
+
+avg_dv_period <- dt[, .(mean_dep_var = mean(dep_var)), by = c('cohort_period','period')] 
+
+# Convert 'cohort_period' to a factor to have discrete color scale in the plot
+avg_dv_period[, cohort_period := as.factor(cohort_period)]
+
+# Black and white friendly
+# Define the linetypes and shapes based on the number of unique cohort_periods
+line_types <- c("solid", "dashed", "dotted", "twodash", "longdash")
+shapes <- c(16, 17, 18, 19, 15)  # Filled shapes
+num_cohorts <- length(unique(avg_dv_period$cohort_period))
+line_types <- line_types[1:num_cohorts]
+shapes <- shapes[1:num_cohorts]
+
+# Plot
+gg <- ggplot(avg_dv_period, aes(x = period, y = mean_dep_var, group = cohort_period)) +
+  geom_line(aes(linetype = cohort_period)) +
+  geom_point(aes(shape = cohort_period)) +
+  geom_vline(data = avg_dv_period[cohort_period == period], aes(xintercept = period, linetype = cohort_period), color = "black") +
+  scale_linetype_manual(values = line_types) +
+  scale_shape_manual(values = shapes) +
+  labs( # title = "Trends in Mean Hours Listened by Cohort Period",
+    x = "Period",
+    y = "Outcome variable",
+    linetype = "Cohort-period",
+    shape = "Cohort-period") +
+  scale_x_continuous(breaks = sort(unique(avg_dv_period$period)))+
+  my_theme() 
+
+# Set color blindness-friendly palette for digital version and print
+gg <- gg + scale_color_brewer(palette = "Dark2", name = "Cohort-period")
+
+print(gg)
+ggsave(paste0(out_dir, 'outcome_by_cohort_period_bw.png'))
+
+unique(dt$covariate)
+unique(dt$cov_effect)
+
+
+# Visualize the treatment effect w/ cov -----------------------------------
+# Per cohort, rel time, and covariate. 
+
+# ETWFE -------------------------------------------------------------------
+dt[, covariate.f := as.factor(covariate)]
+mod_etwfe_pkg =
+  etwfe(
+    fml  = dep_var ~ 0, # outcome ~ controls
+    tvar = period,        # time variable
+    xvar = covariate.f,
+    gvar = cohort_period, # group variable
+    data = dt,       # dataset
+    vcov = ~unit  # vcov adjustment (here: clustered)
+  )
+summary(mod_etwfe_pkg)
+
+emfx(
+  mod_etwfe_pkg,
+  type = c("simple"),
+  by_xvar = T,
+  collapse = "auto")
+
+
+
+# Stacked -----------------------------------------------------------------
+
+
--- analysis/code/sim_data_cov.R
+++ analysis/code/sim_data_cov.R
@@ -16,53 +16,66 @@ library(ggthemes)
 select <- dplyr::select
 set.seed(123)
 
-
 # Initiate the data -----------------------------------------------------------
 # Create a function that takes in the number of units, the number of periods,
 # a treatment effect that will accumulate overtime (tau), and
 # cohort periods (in which periods a cohort is treated). 
 
-calc_dep_var <- function(constant, unit_fe, period_fe, tau_cum, error){
-  dep_var = constant + unit_fe + period_fe + tau_cum + error
+calc_dep_var <- function(constant, unit_fe, period_fe, tau_cum, cov_effect, error){
+  dep_var = constant + unit_fe + period_fe + tau_cum + cov_effect + error
 }
 
-init_data <- function(num_unit, num_period, tau, cohort_periods, constant){
-  end_period = num_period-1 # periods start from 0
-  # Fixed Effects ------------------------------------------------
-  # unit fixed effects
+init_data <- function(num_unit, num_period, tau, cohort_periods, constant, include_covariate = FALSE, interaction_term = 0){
+  end_period = num_period - 1 # periods start from 0
+  
+  # Generate unit fixed effects
   unit <- tibble(
     unit = 1:num_unit, 
     unit_fe = rnorm(num_unit, 0, .1),
-    # Assign units to different treatment cohorts
-    cohort_period = sample(cohort_periods, num_unit, replace = TRUE), 
-    # generate treatment effect
-    mu = rnorm(num_unit, tau, 0.2))
+    cohort_period = sample(cohort_periods, num_unit, replace = TRUE),
+    mu = rnorm(num_unit, tau, 0.2)
+  )
   
-  # period fixed effects 
+  # Optionally add a covariate
+  if (include_covariate) {
+    unit <- unit %>%
+      mutate(covariate = sample(c(0, 1), size = num_unit, replace = TRUE, prob = c(0.5, 0.5))) # Adjust probabilities as needed
+  } else {
+    unit <- unit %>%
+      mutate(covariate = 0) # If not including covariate, set it to 0
+  }
+  
+  # Generate period fixed effects
   period <- tibble(
     period = 0:end_period,
-    period_fe = rnorm(num_period, 0, .1))
+    period_fe = rnorm(num_period, 0, .1)
+  )
   
-  # Trend Break -------------------------------------------------------------
+  # Create the main dataset with the full interaction of unit X period
+  tot_num_obs = num_unit * num_period
   
-  # make main dataset
-  # full interaction of unit X period 
-  tot_num_obs = num_unit*num_period 
+  # Generate the main dataset
+  data <- expand_grid(unit = 1:num_unit, period = 0:end_period) %>%
+    left_join(., unit) %>%
+    left_join(., period) %>%
+    mutate(
+      error = rnorm(tot_num_obs, 0, 10),
+      treat = ifelse(period >= cohort_period, 1, 0),
+      tau = ifelse(treat == 1, mu, 0)
+    ) %>%
+    group_by(unit) %>%
+    mutate(
+      tau_cum = cumsum(tau),
+      cov_effect = interaction_term * covariate # interaction_term is the coef
+    ) %>%
+    ungroup() %>%
+    mutate(
+      dep_var = calc_dep_var(constant, unit_fe, period_fe, tau_cum, cov_effect, error)
+    )
   
-  expand_grid(unit = 1:num_unit, period = 0:end_period) %>% 
-    left_join(., unit) %>% 
-    left_join(., period) %>% 
-    # make error term and get treatment indicators and treatment effects
-    mutate(error = rnorm(tot_num_obs, 0, 10),
-           treat = ifelse(period >= cohort_period, 1, 0),
-           tau = ifelse(treat == 1, mu, 0)) %>% 
-    # calculate cumulative treatment effects
-    group_by(unit) %>% 
-    mutate(tau_cum = cumsum(tau)) %>% 
-    ungroup() %>% 
-    # calculate the dependent variable
-    mutate(dep_var = calc_dep_var(constant, unit_fe, period_fe, tau_cum,error))
+  return(data)
 }
+
 
 # Initiate the data and add heterogeneity in treatment effects (TE) ------------
 # Until now, the dynamics of the TE between the treatment groups are the same
@@ -75,9 +88,10 @@ sim_data <- function(...){
                                   num_period = 10, 
                                   tau = 1.00, 
                                   cohort_periods = c(2,3,4,99,100,101), # c(2,3,4,5,6,7,8)
-                                  constant = constant))
+                                  constant = constant,
+                                  include_covariate = T, 
+                                  interaction_term = 3))
   
-  setnames(data, 'dep_var', 'hrs_listened')
   setkeyv(data, c('cohort_period', 'unit', 'period'))
   
   # Introduce heterogeneity in treatment effects 
@@ -87,7 +101,7 @@ sim_data <- function(...){
   setkeyv(data, c('unit', 'period')) # order
   data[cohort_period==2, tau_cum := cumsum(tau), by = unit]
   # calculate the dependent variable
-  data[cohort_period==2, hrs_listened := calc_dep_var(constant,unit_fe,period_fe,tau_cum,error)]
+  data[cohort_period==2, dep_var := calc_dep_var(constant,unit_fe,period_fe,tau_cum,cov_effect,error)]
   
   # Introduce heterogeneity in treatment effects 
   # calculate the new tau
@@ -96,7 +110,7 @@ sim_data <- function(...){
   setkeyv(data, c('unit', 'period')) # order
   data[cohort_period==3, tau_cum := cumsum(tau), by = unit]
   # calculate the dependent variable
-  data[cohort_period==3, hrs_listened := calc_dep_var(constant,unit_fe,period_fe,tau_cum,error)]
+  data[cohort_period==3, dep_var := calc_dep_var(constant,unit_fe,period_fe,tau_cum,cov_effect,error)]
   
   setkeyv(data, c('unit', 'period'))
   
@@ -104,31 +118,3 @@ sim_data <- function(...){
 }
 
 
-sim_data <- function(num_unit = 1000, num_period = 10, tau = 1.00, cohort_periods = c(2, 3, 4, 99, 100, 101), 
-                     constant = 80, interaction_term = 0, include_covariate = FALSE) {
-  
-  # ... existing code for generating 'unit' and 'period' data ...
-  
-  # Add a covariate column if include_covariate is TRUE
-  if (include_covariate) {
-    unit <- unit %>% 
-      mutate(covariate = rnorm(num_unit, mean = 0, sd = 1)) # Or use a different distribution/mean/sd as needed
-  } else {
-    unit <- unit %>% 
-      mutate(covariate = 0) # Include the covariate column but set all values to 0
-  }
-  
-  # ... existing code for merging 'unit' and 'period' data ...
-  
-  # Modify treatment effect based on the covariate if include_covariate is TRUE
-  data <- data %>%
-    mutate(tau = ifelse(treat == 1, mu + (include_covariate * covariate * interaction_term), 0)) %>%
-    group_by(unit) %>%
-    mutate(tau_cum = cumsum(tau)) %>%
-    ungroup() %>%
-    mutate(hrs_listened = calc_dep_var(constant, unit_fe, period_fe, tau_cum, error))
-  
-  # ... rest of the existing code ...
-  
-  return(data)
-}
